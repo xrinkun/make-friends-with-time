f115b36779ffd17b03e8c8ca4391a5918a7e108b
xrinkun <xrinkun00@gmail.com>
2021-05-24 18:24:22 +0800

    Hello Boss Zhu

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="content">朱总，您好</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  method: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  font-size: 6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
